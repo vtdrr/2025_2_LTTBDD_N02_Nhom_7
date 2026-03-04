376d5cd443dc1b2c91f20db23c2896b65368be96
TranDatt-labo <23010128@st.phenikaa-uni.edu.vn>
2026-03-04 18:58:19 +0700

    chan trang

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,40 @@
+import 'FoodScreen.dart';
+import 'Profile.dart';
+import 'TrainScreen.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const TrainScreen(),
+    const FoodScreen(),
+    const Profile(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        backgroundColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.food_bank), label: 'Dinh dưỡng'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Cài đặt'),
+        ],
+      ),
+    );
+  }
+}
